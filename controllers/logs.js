581c55141d62a4187c7d7c6d5fd37362b19a992e
Jake <mjakerick@gmail.com>
2019-05-20 14:26:32 -0700

    Added GET route to logs controller

--- controllers/logs.js
+++ controllers/logs.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
 const Logs = require('../models/logs.js');
+
+router.get('/', (req, res) => {
+  Logs.find({}, (error, foundLog) => {
+    res.json(foundLog);
+  });
+});
 
 module.exports = router;
